b28713b4ed78358d626ed46ee96e38f9ddcf8c46
Marcello Orru <marcelorru@gmail.com>
2024-08-04 18:31:54 +0200

    Popolazione Tabelle
    - Creazione Seeder per inserimento anno 2024

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Transaction;
+use Carbon\Carbon;
 
 class TransactionSeeder extends Seeder
 {
@@ -13,5 +15,17 @@ class TransactionSeeder extends Seeder
     public function run(): void
     {
         //
+        $year = 2024;
+        $startDate = Carbon::createFromDate($year,1,1);
+        $endDate = Carbon::createFromDate($year,12,31);
+
+        while($startDate->lte($endDate)){
+            Transaction::create([
+               'amount' => 0.00,
+                'date' => $startDate->toDateString(),
+                'description' => '', // Descrizione iniziale vuota 
+            ]);
+            $startDate->addDay();
+        }
     }
 }
